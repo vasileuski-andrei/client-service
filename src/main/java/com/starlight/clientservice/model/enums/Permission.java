d02974435d79138fbebcdebca8c06c144f744c7a
Andrei Vasileuski <vasileuski.andrei@gmail.com>
2022-05-18 11:50:15 +0300

    add role and permissions

---
+++ src/main/java/com/starlight/clientservice/model/enums/Permission.java
@@ -0,0 +1,16 @@
+package com.starlight.clientservice.model.enums;
+
+public enum Permission {
+    USER_READ("user:read"),
+    USER_WRITE("user:write");
+
+    private final String permission;
+
+    Permission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+}
