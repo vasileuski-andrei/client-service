5e237962a62b12091a600b8dc54c82c8b6c01dd8
Andrei Vasileuski <vasileuski.andrei@gmail.com>
2022-05-18 14:03:18 +0300

    refactor SecurityConfig

--- src/main/java/com/starlight/clientservice/security/SecurityConfig.java
+++ src/main/java/com/starlight/clientservice/security/SecurityConfig.java
@@ -1,16 +1,14 @@
 package com.starlight.clientservice.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,28 +17,38 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final JwtConfig jwtConfig;
+
+    @Autowired
+    public SecurityConfig(JwtConfig jwtConfig) {
+        this.jwtConfig = jwtConfig;
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-                .csrf()
-                .disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
+                .antMatchers("/api/auth/login").permitAll()
+                .anyRequest()
+                .authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login").permitAll()
-                .and()
-                .logout()
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .deleteCookies("JSESSIONID");
+                .apply(jwtConfig);
 
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public AuthenticationManager authenticatonManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS users(
     id BIGSERIAL PRIMARY KEY,
     email VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(120) NOT NULL,
-    creation_date DATE NOT NULL
+    creation_date DATE NOT NULL,
+    role VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS client(
