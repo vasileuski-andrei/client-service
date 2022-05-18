8fa98fbd5bd9d3c5f7e99893a63ccc1cae539058
Andrei Vasileuski <vasileuski.andrei@gmail.com>
2022-05-18 13:33:30 +0300

    add SecurityUser and UserDetailsService

---
+++ src/main/java/com/starlight/clientservice/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.starlight.clientservice.security;
+
+import com.starlight.clientservice.model.User;
+import com.starlight.clientservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailsServiceImpl")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(email);
+        return SecurityUser.fromUser(user);
+    }
+}
